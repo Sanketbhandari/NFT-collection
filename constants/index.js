0e70995b63080288e669b46cca7e7815b2a16002
Sanket Bhandari <sanketbhandari22@gmail.com>
2023-03-19 09:58:56 +0530

    project nft collection updated

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xcc1161Be2770d879240EE83d662eabA516E0cCEF";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
